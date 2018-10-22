4cc5fbc283c6da93ca14f4c0973ac1e481a7ffab
wolff69 <44376249+wolff69@users.noreply.github.com>
2018-10-22 13:43:31 -0400

    uploaded script example library
    uploaded script example library to show basic prolog language syntax and some basic query functionality.

---
+++ prgrm2.pl
@@ -0,0 +1,8 @@
+change_directory('H:\DSP\PL').
+[prgrm1].
+phoneno(alex,3604901696).
+phoneno(donato,3157862405).
+teaches(draper,MTH125).
+teaches(donato,CIS221).
+teaches(jackson,CIS116).
+studies(henry,CIS116).
